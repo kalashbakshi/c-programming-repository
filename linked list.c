0dad644c3d0525627c5f81229b75765ccdbdb3e8
kalashbakshi <120623858+kalashbakshi@users.noreply.github.com>
2023-05-20 23:29:54 +0530

    Add files via upload

---
+++ linked list.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+#include<stdlib.h
+
+struct node
+{
+
+int data;
+
+struct node "next;
+};
+
+struct node "head;
+
+void beginsert 0;
+
+void lastinset 0:
+
+void randominserto;
+
+void begin_delete();
+
+void last delete,
+
+void random_delete();
+
+void display 0:
+
+void search0:
+
+void main()
+{
+
+int choice=0
